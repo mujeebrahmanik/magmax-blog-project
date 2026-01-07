32115b3d43d4617317570dcf41c5bc6aef05cdbc
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-01-07 14:55:20 +0400

    image changes  settings

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,26 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-    devIndicators: {
-      autoPrerender: false,
-      buildActivity: false,
-    },
+  devIndicators: {
+    autoPrerender: false,
+    buildActivity: false,
+  },
   images: {
     remotePatterns: [
-      
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/**',
-      },
       {
         protocol: 'https',
         hostname: 'magmax-blog-project.onrender.com',
@@ -28,8 +14,8 @@ const nextConfig = {
         pathname: '/media/**',
       },
     ],
-    unoptimized: true,
+    unoptimized: true, 
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
